f61c32f37b39da36a533ada84356a4dbd7d0098a
niteshyadu9719-bit <niteshyadu9719@gmail.com>
2026-08-30 18:21:15 +0530

    Add files via upload

---
+++ q41.c
@@ -0,0 +1,31 @@
+/*
+* Name : Nitesh Yadav
+* Sap Id : 590043204
+* Day : 21 Question : 41
+* Date : 30-08-2026
+*
+* PROBLEM  STATEMENT :
+* <Write a program to swap the first and last digit of a number.>
+*/
+#include <stdio.h>
+
+int main(void) {
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        } 
+    }
+
+    if (sum == num) {
+        printf("%d is a perfect number.\n", num);
+    } else {
+        printf("%d is not a perfect number.\n", num);
+    }
+
+    return 0;
+}
